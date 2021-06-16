d88724e84569ab03a5373d92d7dab294006056bd
Wessel van Kasteel <s1115662@student.hsleiden.nl>
2021-06-16 11:06:13 +0200

    Added Header.js and Menu.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Header from './components/Header/Header';
+
 import Login from './components/Auth/Login/Login';
 import Register from './components/Auth/Register/Register';
 import UserProfile from './components/UserProfile/UserProfile';
@@ -11,8 +13,9 @@ import SearchVacancy from './components/Search/Vacancy/SearchVacancy';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
-    return(
+    return (
         <Router>
+            <Header />
             <Route path="/zoek-vacature" component={SearchVacancy} />
             {/* <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
